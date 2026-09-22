8421e24d4ca2532635906e8494f7b8be0bdfc5a9
Martin <martinjavourezb2b@gmail.com>
2026-09-22 10:35:32 +0200

    Implement factorial functions recursively and iteratively
    Added recursive and iterative implementations for factorial calculation.

--- Algorithme/Récursivité/activités_récursives/Fonction_récur_activité.py
+++ Algorithme/Récursivité/activités_récursives/Fonction_récur_activité.py
@@ -1,1 +1,32 @@
+def h(x):
+    return x+1
+def g(x):
+    return h(x)+2+k(x+1)
+def f(x):
+    return g(x)+1
+def k(x):
+    return x+1
 
+def factorielle(n):
+    assert n>=0
+    if n==1:
+        return 1
+    else:
+        return n*factorielle(n-1)
+    
+def fact_iterative(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact = fact * i
+    return fact
+
+def fact_recursive(n):
+    assert n >= 0
+    if n==1:
+        return 1
+    else:
+        return n*fact_recursive(n-1)
+
+#Q7/8/9
+fact_iterative(1000)
+fact_recursive(1000)
